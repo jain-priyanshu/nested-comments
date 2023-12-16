0296c0844cdc4519018a825d48959b843fb6a7ce
Priyanshu Jain <jainpriyanshu733@gmail.com>
2023-12-16 13:04:39 +0530

    Slim Server Setup

---
+++ server/public/index.php
@@ -0,0 +1,16 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// home test
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello World!");
+    return $response;
+});
+
+$app->run();
